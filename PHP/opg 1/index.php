0da4af4ebecc8153c86c5e5a737f19ac5f005284
Alessander Anthony <2174951@talnet.nl>
2023-05-08 11:20:21 +0200

    Uitwerking van de exercise

---
+++ PHP/opg 1/index.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "op 1. <br>";
+
+$num1 = 12;
+$num2 = 10;
+echo "Het resultaat is " . $num1 + $num2;
+
+echo "<br><br> op 2. <br>";
+
+echo "Het resultaat is " . $num1 * $num2;
+
+echo "<br><br> op 3. <br>";
+
+$naam = "Alessander";
+
+echo "Welkom " . $naam;
+
+echo "<br><br> op 4.<br>";
+
+
+// deze code bevat veel fouten haal alle fouten eruit!
+//
+
+$mijnNaam = "Carl";
+$jouwNaam = "Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
