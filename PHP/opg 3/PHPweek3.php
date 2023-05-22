0d3437a2b2c71966e7745e5339430f1ad6b6e859
Alessander Anthony <2174951@talnet.nl>
2023-05-22 10:41:19 +0200

    Uitwerking van de eercise

---
+++ PHP/opg 3/PHPweek3.php
@@ -0,0 +1,44 @@
+<?php
+
+//deel 1
+// $myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+// $myArray[]='helicopter';
+
+// echo $myArray[0];
+// echo '<br>';
+// echo $myArray[1];
+// echo '<br>';
+// echo $myArray[2];
+// echo '<br>';
+// echo $myArray[3];
+// echo '<br>';
+// echo $myArray[4];
+// echo '<br>';
+// echo $myArray[5];
+// echo '<br>';
+// echo $myArray[6];
+
+//deel 2
+// $myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+// echo 'het array heeft '.count($myArray).' elementen' ;
+
+// $myArray[]='helicopter';
+// echo '<br>het array heeft '.count($myArray).' elementen' ;
+
+
+//deel 3 
+// 1. bar
+// 2. 4
+// 3. toyota
+// 4. 3
+// 5. 5
+// 6. five
+
+
+//deel 4A
+// $gemiddelde = array_sum($cijfersPHP) / count($cijfersPHP);
+// echo 'Gemiddelde is: '.(round($gemiddelde,1));
+
+//deel 4B
+// Ik denk dat het beter is om de regels niet samen te voegen, omdat je code dan korter is
+?>
